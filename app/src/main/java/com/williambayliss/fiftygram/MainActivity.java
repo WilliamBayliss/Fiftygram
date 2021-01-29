11e184238268137be4ceff8efd5018e290c9e4ea
williambayliss <wmbayliss@gmail.com>
2021-01-29 11:12:33 -0500

    Added more image filters, added save photo button

--- app/src/main/java/com/williambayliss/fiftygram/MainActivity.java
+++ app/src/main/java/com/williambayliss/fiftygram/MainActivity.java
@@ -1,39 +1,63 @@
 package com.williambayliss.fiftygram;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.Transformation;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
 
+import jp.wasabeef.glide.transformations.gpu.BrightnessFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.ContrastFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.InvertFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.PixelationFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SwirlFilterTransformation;
 import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.VignetteFilterTransformation;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     private ImageView imageView;
     private Bitmap image;
+    private Bitmap filteredImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         imageView = findViewById(R.id.image_view);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void choosePhoto(View v) {
@@ -48,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
                 .load(image).apply(RequestOptions
                 .bitmapTransform(filter))
                 .into(imageView);
+        imageView.setDrawingCacheEnabled(true);
+        filteredImage = imageView.getDrawingCache();
     }
 
     public void applySepia(View v) {
@@ -61,6 +87,31 @@ public class MainActivity extends AppCompatActivity {
     public void applySketch(View v) {
         apply(new SketchFilterTransformation());
     }
+
+    public void applyContrast(View v) {
+        apply(new ContrastFilterTransformation());
+    }
+
+    public void applyPixel(View v) {
+        apply(new PixelationFilterTransformation());
+    }
+
+    public void applySwirl(View v) {
+        apply(new SwirlFilterTransformation());
+    }
+
+    public void applyInvert(View v) {
+        apply(new InvertFilterTransformation());
+    }
+
+    public void applyBright(View v) {
+        apply(new BrightnessFilterTransformation());
+    }
+
+    public void applyVignette(View v) {
+        apply(new VignetteFilterTransformation());
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
